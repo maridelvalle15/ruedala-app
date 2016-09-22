50c539facf11372193bb71e0fe19b3fb2ae7cab1
Marisela Del Valle <marisela.delvalle93@gmail.com>
2016-09-21 23:51:26 -0400

    Biciescuela y prestamos en admin django

--- reportes/admin.py
+++ reportes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from reportes.models import *
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Prestamos)
+admin.site.register(Biciescuelas)
