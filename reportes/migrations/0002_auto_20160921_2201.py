856d696f5f9cdb4c8c174dbfbf6ed7e189eeedb8
Marisela Del Valle <marisela.delvalle93@gmail.com>
2016-09-22 00:03:55 -0400

    Agregada opcion sin redireccion para buscar biciescuela o prestamo

---
+++ reportes/migrations/0002_auto_20160921_2201.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reportes', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Biciescuelas',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('sabe_manejar', models.CharField(max_length=2, choices=[(b'Si', b'Si'), (b'No', b'No')])),
+                ('fecha', models.DateTimeField()),
+                ('aprobado', models.CharField(max_length=2, choices=[(b'Si', b'Si'), (b'No', b'No')])),
+                ('pago_carnet', models.CharField(max_length=2, choices=[(b'Si', b'Si'), (b'No', b'No')])),
+                ('instructor', models.CharField(max_length=100)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Usuario',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nombre', models.CharField(max_length=100)),
+                ('apellido', models.CharField(max_length=100)),
+                ('identificacion', models.CharField(max_length=100)),
+                ('telefono', models.CharField(max_length=100)),
+                ('correo', models.EmailField(max_length=254)),
+            ],
+        ),
+        migrations.RemoveField(
+            model_name='prestamos',
+            name='apellido',
+        ),
+        migrations.RemoveField(
+            model_name='prestamos',
+            name='identificacion',
+        ),
+        migrations.RemoveField(
+            model_name='prestamos',
+            name='nombre',
+        ),
+        migrations.RemoveField(
+            model_name='prestamos',
+            name='telefono',
+        ),
+        migrations.AddField(
+            model_name='biciescuelas',
+            name='usuario',
+            field=models.ForeignKey(default=None, to='reportes.Usuario'),
+        ),
+        migrations.AddField(
+            model_name='prestamos',
+            name='usuario',
+            field=models.ForeignKey(default=None, to='reportes.Usuario'),
+        ),
+    ]
